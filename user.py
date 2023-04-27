541a9a716e24d18cff720a2d75abb7f4b761a851
AmrAbrazek <amr.3brazek@gmail.com>
2023-04-27 23:53:42 +0200

    encrypt password and get it

--- user.py
+++ user.py
@@ -2,14 +2,14 @@ import re
 import getpass
 from cryptography.fernet import Fernet
 class User :
-    key = Fernet.generate_key()
-    f = Fernet(key)
     def __init__(self):
         self.first = None
         self.last = None
         self.email = None
         self.number = None
         self.password = None
+        self.key = None
+        self.f = None
 
     def getfirst (self):
         while True:
@@ -54,8 +54,8 @@ class User :
                 continue
     
     def getpassword (self):
-        key = Fernet.generate_key()
-        f = Fernet(key)
+        # key = Fernet.generate_key()
+        # f = Fernet(key)
         while True:
             password = getpass.getpass(prompt='Enter your password: ')
 
@@ -63,18 +63,20 @@ class User :
                 
                 password2 = getpass.getpass(prompt='Renter your password: ')
                 if password2 == password: 
-                    
-                    self.password = User.encryptPassword(password, f)
-                    
-                    print(self.password)
 
-                    self.password = User.decryptPassword(self.password, f)
+                    self.password = password
+                    
+                    # self.password = User.encryptPassword(password, f)
+                    
+                    # print(self.password)
 
-                    print(self.password)
+                    # self.password = User.decryptPassword(self.password, f)
+
+                    # print(self.password)
 
                     break
                 else:
-                    print("passwords do not match")
+                    print("passwords don't match")
                     continue
             else:
                 print("password should be 8 characters at least ")
@@ -83,13 +85,46 @@ class User :
     
     def register (self):
         # self.getfirst()
+        # # print (self.first)
         # self.getlast()
         # self.getemail()
         # self.getnumber()
+        key = Fernet.generate_key()
+        f = Fernet(key)
+        self.key = key
+        self.f = f
+        print (self.f)
+        print (self.key)
         self.getpassword()
-        return self.first, self.last, self.email, self.number, self.password
+        with open("save.txt", "ab") as file:
+                # file.write(str(self.first) + ":" + str(self.last) + ":" + str(self.email) + ":" + str(self.number) + ":" + str(self.encryptPassword(self.password, f)) +"\n")
+                file.write(self.encryptPassword(self.password))
+        
+    
+    def login(self):
+        print (self.key)
+        with open('save.txt', 'rb') as file:
+            encrypted_key = file.read()
+            print (encrypted_key)
+        key = self.key
+        print (key)
+        f = Fernet(key)
+        # f = self.f
 
+        plain_text = f.decrypt(encrypted_key)
+        plain_text = plain_text.decode()
 
+        print(f"decrepted value: {plain_text}")
+
+        # email = input("Email: ")
+        # password = input("Password: ")
+
+        # with open("password.txt", "rb") as file:
+        #     for line in file:
+        #         line=line.split(":")
+        #         if line[3] == email and line[5] == password:
+
+    
     @staticmethod
     def verifyName (name):
         if re.match(r'^[a-zA-Z]+$', name):
@@ -120,13 +155,17 @@ class User :
         else:
             return False
     
-    @staticmethod
-    def encryptPassword (password, f):
-        encrypted_password = f.encrypt(password.encode())
+    def encryptPassword (self, password):
+        f = Fernet(self.key)
+        f = self.f
+        password=str(password).encode()
+        encrypted_password = f.encrypt(password)
         return encrypted_password
     
     @staticmethod
     def decryptPassword (encrypted_password, f):
+        
+
         decrypted_password = f.decrypt(encrypted_password).decode()
         return decrypted_password
     
@@ -139,16 +178,19 @@ class User :
                 print(obj.name, obj.queue)
                 return obj
     
-    @classmethod
-    def save(cls):
-        with open("save.txt", "w") as f:
-            for obj in cls.instances:
-                f.write(str(obj.name) + ":" + str(obj.queue) + ":" + str(obj.size) + ":" +"\n")
+    # @classmethod
+    # def save(cls):
+    #     print (cls.first)
+    #     with open("save.txt", "a") as file:
+    #             file.write(str(cls.first) + ":" + str(cls.last) + ":" + str(cls.email) + ":" + str(cls.number) + ":" + str(cls.encryptPassword(cls.password, cls.f)) +"\n")
 
 
 
 
 user1 = User()
 user1.register()
+print(user1.key)
+user1.login()
+# user1.save()
 
-print (user1.password)
+# print (user1.password)
