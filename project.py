5a612a0e8f15c8fc6551752d8bd535209ccc2ed1
AmrAbrazek <amr.3brazek@gmail.com>
2023-04-29 00:27:24 +0300

    adding view projects method

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 import re
-from user.py import User 
+from tabulate import tabulate
+from datetime import datetime
+# from user.py import User 
 
 # user1 = User()
 # user1.start()
@@ -8,6 +10,7 @@ class project :
     def __init__(self):
         self.projectName = None
         self.authour = None
+        self.authouremail = None
         self.description = None
         self.Target = None
         self.startDate = None
@@ -16,7 +19,7 @@ class project :
     def getfirst (self):
         while True:
             name = input("Project Name: ").strip()
-            if User.verifyName(name):
+            if self.verifyName(name):
                 self.projectName = name
                 break
             else:
@@ -34,13 +37,88 @@ class project :
                         self.authour = Fname + " " + Lname
                         break
     
+
     def getdescription (self):
         self.description = input("Please enter a description: ")
 
 
+    def getProjectProp(self):
+
+        while True:
+            Target = input("Please enter a Target: ")
+            if self.verifyNumber(Target):
+                self.Target = Target
+                break 
+            else:
+                print("Please enter a valid Target, it can't start with a number")
+                continue
+        
+        while True:
+            startDate = input("Please enter a Start Date: ")
+            if self.is_valid_date(startDate):
+                self.startDate = startDate
+                break
+            else:
+                print("Please enter a valid Start Date, it should be in this format (YYYY-MM-DD)")
+                continue
+
+        while True:
+            endDate = input("Please enter a End Date: ")
+            if self.is_valid_date(endDate):
+                self.endDate = endDate
+                break
+            else:
+                print("Please enter a valid End Date, it should be in this format (YYYY-MM-DD)")
+                continue
+
+    
+    def create_project (self):
+        self.getfirst()
+        self.getauthor("amr.3brazek@gmail.com")
+        self.getdescription()
+        self.getProjectProp()
+        with open('projects.txt', 'a') as file:
+            file.write(self.projectName + ":" + self.authour + ":" + self.description + ":" + self.Target + ":" + self.startDate + ":" + self.endDate + "\n")
+    
+    @classmethod
+    def view (cls):
+        with open('projects.txt', 'r') as file:
+            projectsnames = []
+            authors = []
+            decriptions = []
+            maoney = []
+            sdates = []
+            edates = []
+            for line in file.readlines():
+                projectsnames.append(line.split(':')[0])
+                authors.append(line.split(':')[1])
+                decriptions.append(line.split(':')[2])
+                maoney.append(line.split(':')[3])
+                sdates.append(line.split(':')[4])
+                edates.append(line.split(':')[5])
+
+            table = [projectsnames,authors,decriptions,maoney,sdates,edates]
+            print(tabulate(table))
 
 
+    
+                
 
+    @staticmethod
+    def is_valid_date(date_string):
+        try:
+            datetime.strptime(date_string, '%Y-%m-%d')
+            return True
+        except ValueError:
+            return False
+    
+
+    @staticmethod
+    def verifyNumber (number):
+        if re.match(r'^[0-9]+$', number):
+            return True
+        else:
+            return False
 
     
     @staticmethod
@@ -50,3 +128,13 @@ class project :
         else:
             return False
 
+
+
+# if __name__ == "__main__":
+# project_1 = project()
+# project_1.create_project()
+
+project.view()
+
+# table = [["Sun",696000,1989100000],["Earth",6371,5973.6],["Moon",1737,73.5],["Mars",3390,641.85]]
+# print(tabulate(table))
